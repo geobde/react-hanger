b81ee5e31c6e976b9237033857e17d98ab45472d
Andrey Los <puha212@gmail.com>
2020-08-25 01:14:39 +0200

    microbundle

--- copyPackageJsonAndReadme.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-fs.copyFileSync('package.json', './lib/package.json');
-fs.copyFileSync('README.md', './lib/README.md');
-fs.copyFileSync('README-ARRAY.md', './lib/README-ARRAY.md');
